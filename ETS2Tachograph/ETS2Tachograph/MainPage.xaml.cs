d1e3b6226299f903e843c4f6da10b2a09f92b832
WEBER Tobias <tobias.weber@hexagon.com>
2022-02-25 12:43:42 +0100

    set up main page scaffolding

--- ETS2Tachograph/ETS2Tachograph/MainPage.xaml.cs
+++ ETS2Tachograph/ETS2Tachograph/MainPage.xaml.cs
@@ -2,19 +2,41 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //    count++;
+    //    CounterLabel.Text = $"Current count: {count}";
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-		CounterLabel.Text = $"Current count: {count}";
+    //    SemanticScreenReader.Announce(CounterLabel.Text);
+    //}
 
-		SemanticScreenReader.Announce(CounterLabel.Text);
-	}
+    private void OnDriveClicked(object sender, EventArgs e)
+    {
+        // state machine -> drive
+    }
+
+    private void OnWorkClicked(object sender, EventArgs e)
+    {
+        // state machine -> work
+    }
+
+    private void OnStandbyClicked(object sender, EventArgs e)
+    {
+        // state machine -> standby
+    }
+
+    private void OnRestClicked(object sender, EventArgs e)
+    {
+        // state machine -> rest
+    }
+
+    private void OnSettingsClicked(object sender, EventArgs e)
+    {
+        // open settings component
+    }
 }
-
